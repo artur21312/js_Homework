a22b72a23bf11a2beaf58b71258abdf59fedbe3a
artur14 <arturavorskij14@gmail.com>
2024-08-01 21:37:05 +0300

    h2 extreme

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 'use strict';
-let name ='john';
-let admin = name;
+let name, admin;
+name = 'john';
+admin = name;
 alert(admin);
 
 let planet;
 let user;
 
-let kolos = "10" + 10;
-console.log(kolos);
-console.log(typeof kolos);
+let colors = "10" + 10;
+console.log(colors);
+console.log(typeof colors);
