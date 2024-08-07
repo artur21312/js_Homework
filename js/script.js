7a97ddc52860bf43f03a7080bc4832b1992f70d0
artur14 <arturavorskij14@gmail.com>
2024-08-07 22:10:20 +0300

    Hw_3g

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
-let zero= prompt('a','4');
-let one= prompt('b','67');
+const zero= prompt('a','4');
+const one= prompt('b','67');
 console.log(zero+one);
 console.log(zero-one);
 console.log(zero*one);
