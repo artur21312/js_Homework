baf644435c5e2880f55756d3e6acea3945a4ec00
artur14 <arturavorskij14@gmail.com>
2024-08-07 22:01:47 +0300

    hv_4 l

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 'use strict';
 
-let name= prompt('What is your name');
-let age= prompt('How old are you');
-let likesProgramming=confirm ('Do you like the program');
+const name= prompt('What is your name');
+const age= prompt('How old are you');
+const likesProgramming=confirm ('Do you like the program');
 console.log(name,age,likesProgramming);
 console.log(typeof(name));
 console.log(typeof(likesProgramming));
 console.log(typeof(age));
 const greeting = (`привіт  ${name}`);
 alert(greeting);
-let numericage;
-numericage = Number(age);
+const numericage= Number(age);
 let nextAge = 1 + numericage;
 console.log(nextAge);
 const isAdult = 18 <=age;
