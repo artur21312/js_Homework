d232f90805772fa1a88c0d09de1599a9efbcd9c1
artur14 <arturavorskij14@gmail.com>
2024-08-08 21:26:05 +0300

    Hw_8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
-const time = prompt('вкажіть кількість год' )
-let minute = time *60;
-let seconds = minute * 60;
-alert('кількість секунд' + seconds );
+let first = prompt('ведіть число 1' );
+let second = prompt('ведіть число 2');
+let third =prompt('ведіть число 3' );
+const sum = Number(first) + Number(second) + Number(third);
+const arithmetic = sum  / 3;
+alert('середне арифметичне '+ arithmetic);
