e4bd0a9fda150481edfe8f5b28931aaf5ac175d1
artur14 <arturavorskij14@gmail.com>
2024-08-09 17:16:50 +0300

    Hw_8 w

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
-let first = prompt('ведіть число 1' );
-let second = prompt('ведіть число 2');
-let third =prompt('ведіть число 3' );
+const first = prompt('ведіть число 1' );
+const second = prompt('ведіть число 2');
+const third =prompt('ведіть число 3' );
 const sum = Number(first) + Number(second) + Number(third);
 const arithmetic = sum  / 3;
 alert('середне арифметичне '+ arithmetic);
