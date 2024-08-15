9f56fd9cb01c12a91cd3cd0c58572f4dd11c30f5
artur14 <arturavorskij14@gmail.com>
2024-08-15 23:05:18 +0300

    Hw_10 hw

--- js/script.js
+++ js/script.js
@@ -1,45 +1,19 @@
-'use strict';
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
 
-const age = prompt('сколько вам лет');
-const conditionage=age!== null && age.trim().length && !isNaN(+age) ;
-const city=prompt('где вы проживаете');
-const conditioncity=city!== null && city.trim().length && !isNaN(+city);
-const sport=prompt('какой твой льбимый спорт');
-const conditionsport=sport!== null && sport.trim().length && !isNaN(+sport);
 
-if (conditionage){
-    alert(age);
+switch (true){
+    case numOrStr === null:
+        console.log('ви скасували');
+    break;
+    case numOrStr.trim() === '':
+    console.log('Empty String');
+    break;
+    case isNaN( +numOrStr ):
+    console.log(' number is Ba_NaN');
+    break;
+    default:
+    console.log('OK!');
+    break;
 }
-else {
-    alert('Шкода, що Ви не захотіли ввести свій(ю) дату народження');
-}
-if (!conditioncity){
-    alert('Шкода, що Ви не захотіли ввести свіє місто');
-}else if (city==='Київ'||city==='Вашингтон'||city==='Лондон'){
-    alert('Ти живеш у столиці '+city);
-}else if (city===isNaN(+city)){
-    alert('ти живеш у місті '+city);
-}
-
-
-if (!conditionsport) {
-    alert('Шкода, що Ви не захотіли ввести свій спорт');
-}
-
-
-
-
-if (sport === 'бокс') {
-    alert('Круто! Хочеш стати Александр Усик');
-} else if (sport === 'волейбол') {
-    alert('Круто! Хочеш стати Вильфредо Леона');
-} else if (sport === 'тенис') {
-    alert('Круто! Хочеш стати Джон Пий Боланд');
-} else if(sport===isNaN(+sport)){
-    alert('Крутий вид спорту');
-}
-
-
-
-
