8088c27684aea1f48b0c9aa3aa566ff1e104b40c
artur14 <arturavorskij14@gmail.com>
2024-08-20 22:10:52 +0300

    hw 14

--- src/js/script.js
+++ src/js/script.js
@@ -1,72 +1,61 @@
 'use strict';
 
-
-let lop ='';
-for (let sap=10; sap<=20; sap++){
-lop+= sap+',';
-}
-console.log(lop);
-
-for (let sap=10; sap<=20; sap++){
-    let box=sap*sap;
-    console.log(box);
+for (let namber=20 ;namber<=30;namber+=0.5){
+ console.log(namber);
 }
 
-for (let nam =1 ;nam<=10;nam++){
-    const seven=7;
-    let multiplication=nam*seven;
-    console.log(nam+'*'+seven+'='+multiplication);
+for (let namber=10 ;namber<=100;namber+=10){
+    let grivna=namber*27;
+
+    console.log(namber+' доларив '+grivna+' гривень ');
 }
 
-let samer=0;
-for (let name =1 ;name<=15;name++){
-    samer += name;
+const name= prompt('ведитьчисло n')
+for (let namber=1 ;namber<=100;namber++){
+    if (namber>=name){
+        break;
+    }
+    console.log(namber);
 }
-console.log(samer);
 
-let dobutok=15;
-for (let name =16 ;name<=35;name++){
-dobutok*=name;
+let post=0 ;
+if (name<=1){
+    post=0;
+}else if(name===2 || name<=3){
+    post=1;
+}else if(name%2===0||name%3===0){
+    post=0;
+}   else {
+    post=1;
 }
-console.log(dobutok);
-
-let soda=0
-for (let name =1 ;name<=500;name++){
-    soda+=name;
+for (let i = 5; i * i <= name; i += 6) {
+    if (name % i === 0 || name % (i + 2) === 0)  post=0;
 }
-soda=soda/500;
-console.log(soda);
 
-let botl=0;
-for (let name =30 ;name<=80;name++){
-    if (name %2 ===0){
-        botl+=name;
+if (post===1) {
+    console.log(name + " є простим числом.");
+} else {
+    console.log(name + " не є простим числом.");
+}
+
+
+
+const n = prompt('ведить число');
+if (n <= 0) {
+    console.log(false);
+} else {
+
+    let current = 1;
+
+    while (current < n) {
+        current *= 3;
+    }
+
+    if (current === n) {
+        console.log(true);
+    } else {
+        console.log(false);
     }
 }
-console.log(botl);
-
-for (let name =100 ;name<=200;name++){
-    if (name %3 ===0) {
-        console.log(name);
-    }
-}
-
-const mom = prompt('ведите число');
-let solid = 0;
-let asist=0;
-for (let name =1 ;name<=mom;name++) {
-    if (name % 2 === 0) {
-    solid++;
-    asist+= name;
-    }
-}
-console.log('количество чисел '+solid);
-console.log('сума чисел '+asist);
 
 
-for (let name =1 ;name<=10;name++) {
-for (let nam =1 ;nam<=10;nam++){
-    let multiplication=nam*name;
-    console.log(nam+'*'+name+'='+multiplication);
-}
-}
