1831201b3816582752336f5145758a2839a4bbe3
artur14 <arturavorskij14@gmail.com>
2024-08-22 18:52:16 +0300

    hw 15

--- src/js/script.js
+++ src/js/script.js
@@ -1,62 +1,20 @@
 'use strict';
 
-for (let namber=20 ;namber<=30;namber+=0.5){
- console.log(namber);
-}
 
-for (let namber=10 ;namber<=100;namber+=10){
-    let grivna=namber*27;
-
-    console.log(namber+' доларив '+grivna+' гривень ');
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
-const name= prompt('ведитьчисло n')
-for (let namber=1 ;namber<=100;namber++){
-    if (namber>=name){
-        break;
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const length=prompt('ведите число');
+let sum ='';
+const generateKey =  function () {
+    for (let i = 1; i <= length; i++){
+    sum  += characters[getRandomInt(0,characters.length)];
     }
-    console.log(namber);
+    console.log(sum);
 }
-
-let post=0 ;
-if (name<=1){
-    post=0;
-}else if(name===2 || name<=3){
-    post=1;
-}else if(name%2===0||name%3===0){
-    post=0;
-}   else {
-    post=1;
-}
-for (let i = 5; i * i <= name; i += 6) {
-    if (name % i === 0 || name % (i + 2) === 0)  post=0;
-}
-
-if (post===1) {
-    console.log(name + " є простим числом.");
-} else {
-    console.log(name + " не є простим числом.");
-}
-
-
-
-const n = prompt('ведить число');
-if (n <= 0) {
-    console.log(false);
-} else {
-
-    let current = 1;
-
-    while (current < n) {
-        current *= 3;
-    }
-
-    if (current === n) {
-        console.log(true);
-    } else {
-        console.log(false);
-    }
-}
-
-
+console.log(generateKey());
