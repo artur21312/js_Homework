383dc4ec41766f472520a7c84485dea50eb3a20a
artur14 <arturavorskij14@gmail.com>
2024-08-29 21:03:50 +0300

    hw 18

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,85 @@
 'use strict';
 
-const array = [1, 2, 3, 4, 5, 6, 7];
-const nam =prompt('ведите число')-1;
+const  mas=[32,'babl',43,12,false,53,'prit'];
 
-const minus=function (lopi,namber){
-    for (let i=0;i<=lopi.length;i++){
-        if (namber===i){
-            continue;
+const old=function (){
+    let sum=0;
+    let masn=0;
+    for(let i=0;i<mas.length;i++){
+        if(typeof mas[i]==='number'){
+            masn++;
+            sum+=mas[i];
         }
-        console.log(array[i])
+    }
+    let masnn=sum/masn;
+
+    console.log(masnn);
+}
+console.log(old());
+
+const x=+prompt('введите число X');
+const y=+prompt('введите число Y');
+const znake=prompt('введите знак');
+const doMath=function (x, znak, y){
+    let sum=x;
+    if( znak==='+'){
+        sum+=y;
+    }else if( znak==='-'){
+        sum=x-y;
+    }else if( znak==='*'){
+        sum=x*y;
+    }else if( znak==='/'){
+        sum=x/y;
+    }else if( znak==='%'){
+        sum=x%z;
+    }else if( znak==='^'){
+        for (let i=1;i<y;i++){
+            sum=sum*sum;
+        }
+    }
+    console.log(sum);
+}
+console.log(doMath(x,znake,y));
+
+
+const fill2DArray=function () {
+
+    const rows = parseInt(prompt("Введіть кількість рядків (довжина основного масиву):"), 10);
+
+
+    const array = [];
+
+
+    for (let i = 0; i < rows; i++) {
+
+        const cols = parseInt(prompt(`Введіть кількість елементів у рядку ${i + 1}:`), 10);
+
+        const innerArray = [];
+
+        for (let j = 0; j < cols; j++) {
+            const value = prompt(`Введіть значення для елементу [${i + 1}][${j + 1}]:`);
+            innerArray.push(value);
+        }
+
+        array.push(innerArray);
     }
 
+    return array;
 }
 
-console.log(minus(array,nam));
+console.log(fill2DArray());
+
+
+const inputString = prompt("Введіть рядок:");
+const chars = prompt("Введіть символи для видалення :").split('');
+
+function removeCharacters(str, charsToRemove) {
+    for (let char of charsToRemove) {
+        str = str.split(char).join('');
+    }
+    return str;
+}
+
+
+const result = removeCharacters(inputString, chars);
+console.log(result);
