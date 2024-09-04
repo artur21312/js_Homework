cf80521ddbe4376e9ed494db3c8b33306e04b2e0
artur14 <arturavorskij14@gmail.com>
2024-09-04 16:34:07 +0300

    22

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,17 @@
 'use strict';
 
-let masiv=(callback)=>{
-    let mas=[];
-    for(;true;){
-            let primitiv=prompt('ведите значение ');
-            if(primitiv===null||primitiv==='')break;
-            mas.push(primitiv);
-    }
-    callback(mas);
-}
-let sort =(mas)=>{
-console.log(mas);
-mas.sort((a,b)=> a-b);
-console.log(mas);
-mas.splice(1,3);
-console.log(mas);
-}
-masiv(sort);
+const mas= [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+
+const minimum = Math.min(...mas);
+console.log(minimum);
+
+const max = Math.max(...mas);
+console.log(max);
+
+const negative =
+
+
+
+
+
+
