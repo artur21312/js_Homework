ddae1e595fb4b21fe6e0be12df86e5c69bdf1a2e
artur14 <arturavorskij14@gmail.com>
2024-09-04 23:29:08 +0300

    22.1

--- src/js/script.js
+++ src/js/script.js
@@ -1,64 +1,81 @@
 'use strict';
 
-const mas= [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
-const minimum = Math.min(...mas);
-console.log(minimum);
-
-const max = Math.max(...mas);
-console.log(max);
-
-const negative = mas.filter((namber) =>{
-    return namber<0;
-})
-console.log(negative.length);
-
-const pozitive_noparne = mas.filter((namber) =>{
-    return namber>0&&namber%2===1;
-})
-console.log(pozitive_noparne.length);
-
-const pozitive_parne = mas.filter((namber) =>{
-    return namber>0&&namber%2===0;
-})
-console.log(pozitive_parne.length);
-
-const sum=(mas)=>{
-    let suma=0;
-    for(let i=0;i<mas.length;i++){
-        suma+=mas[i];
+let users = [
+    {
+        "index": 0,
+        "isActive": true,
+        "balance": "$2,226.60",
+        "name": "Eugenia Sawyer",
+        "gender": "female",
+        "phone": "+1 (840) 583-3207",
+        "address": "949 John Street, Rose, Puerto Rico, 1857"
+    },
+    {
+        "index": 1,
+        "isActive": true,
+        "balance": "$2,613.77",
+        "name": "Pauline Gallegos",
+        "gender": "female",
+        "phone": "+1 (985) 593-3328",
+        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    },
+    {
+        "index": 2,
+        "isActive": false,
+        "balance": "$3,976.41",
+        "name": "Middleton Chaney",
+        "gender": "male",
+        "phone": "+1 (995) 591-2478",
+        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+    },
+    {
+        "index": 3,
+        "isActive": true,
+        "balance": "$1,934.58",
+        "name": "Burns Poole",
+        "gender": "male",
+        "phone": "+1 (885) 559-3422",
+        "address": "730 Seba Avenue, Osage, Alabama, 6290"
+    },
+    {
+        "index": 4,
+        "isActive": true,
+        "balance": "$3,261.65",
+        "name": "Mcfadden Horne",
+        "gender": "male",
+        "phone": "+1 (942) 565-3988",
+        "address": "120 Scholes Street, Kirk, Michigan, 1018"
+    },
+    {
+        "index": 5,
+        "isActive": false,
+        "balance": "$1,790.56",
+        "name": "Suzette Lewis",
+        "gender": "female",
+        "phone": "+1 (837) 586-3283",
+        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
     }
-    return suma;
+]
+
+
+const filtr=(balanse)=>{
+    return parseFloat(balanse.replace(/[$,]/g, ''));
 }
+const balans =users
+    .filter(user=>filtr(user.balance)>2000)
+    .map(user=>user.phone);
 
-console.log(sum(pozitive_parne));
-
-console.log(sum(pozitive_noparne));
-
-
-const pozitive = mas.filter((namber) =>{
-    return namber>0;
-})
-
-const dobut=(mas)=>{
-    let suma=mas[0];
-    for(let i=1;i<mas.length;i++){
-        suma*=mas[i];
-    }
-    return suma;
-}
-console.log(dobut(pozitive));
-
-const mas_max=(mas)=>{
-    const max=Math.max(...mas);
-    for(let i=0;i<mas.length;i++){
-        if(mas[i]!==max){
-            mas[i]=0;
+const suma=(users)=>{
+    let sum=0;
+    for(let i=0;i<users.length;i++){
+        if (filtr(users[i].balance)>2000){
+        sum+=filtr(users[i].balance);
         }
     }
-    console.log(mas);
+    return sum;
 }
 
-console.log(mas_max(mas));
 
-
+console.log(suma(users));
+console.log(balans);
