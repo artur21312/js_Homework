1c012bc7fcf646b5c2431c0d587e3cce118a6c3a
artur14 <arturavorskij14@gmail.com>
2024-09-04 15:58:07 +0300

    21

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,19 @@
 'use strict';
 
-
-const mainFunction=function (callback) {
-    const nam1 = +prompt();
-    const nam2 = +prompt();
-
-    callback (nam1, nam2);
-}
-const exponentiation =function(nam1,nam2) {
-    let answer=nam1;
-    for(let i=1;i<nam2;i++){
-    answer*=answer;
+let masiv=(callback)=>{
+    let mas=[];
+    for(;true;){
+            let primitiv=prompt('ведите значение ');
+            if(primitiv===null||primitiv==='')break;
+            mas.push(primitiv);
     }
-
-    return alert(answer);
-
+    callback(mas);
 }
-
-const multiplay=function(num1,num2){
-    const answer=num1*num2;
-    return answer;
+let sort =(mas)=>{
+console.log(mas);
+mas.sort((a,b)=> a-b);
+console.log(mas);
+mas.splice(1,3);
+console.log(mas);
 }
-
-const division=function(num1,num2){
-    const answer=num1/num2;
-    return answer;
-}
-
-const modulo=function(num1,num2){
-    const answer=num1%num2;
-    return answer;
-}
-
-console.log(mainFunction(exponentiation));
-console.log(mainFunction(multiplay));
-console.log(mainFunction(division));
-console.log(mainFunction(modulo));
+masiv(sort);
