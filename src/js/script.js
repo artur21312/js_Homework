f780cab9b63fd39bf4cb5927da528b525857ebc2
artur14 <arturavorskij14@gmail.com>
2024-09-22 20:16:14 +0300

    hw28

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,33 @@
 'use strict';
 
-const  user ={
-    name:'alex',
+
+    const user = {
+        name:'alex',
         age:25,
-    funk:function () {
-        console.log('hello '+this.name );
-
     }
-}
 
-user.funk(user);
+    const getFullName = function () {
+        return `${this.name} ${this.age}`
+    }
+
+    const apply = function (func, context, ...args) {
+        let result = null;
+        context.func = func;
+        result = context.func(...args);
+        delete context.func;
+
+        return result;
+    }
+
+    const bind = (func= null, context = undefined, args = []) => {
+        if(!func) return undefined;
+
+        return function() {
+            return apply(func, context, ...args)
+        }
+    }
+
+    const bindedGetFullName = bind(getFullName, user);
+    console.log(bindedGetFullName);
+
+
