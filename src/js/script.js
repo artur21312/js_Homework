485a84c7fc5ea6bde30fb4dacf224a3ca0120f5b
artur14 <arturavorskij14@gmail.com>
2024-09-26 23:46:35 +0300

    hw_33

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,5 @@
 'use strict';
+let randomInt = Math.floor(Math.random() * 10) + 1;
+document.getElementById('126').src = 'images/' + randomInt + 'img_.png';
 
 
-const createNestedElement = (tag, text = '', children = []) => {
-    const element = document.createElement(tag);
-    element.textContent = text;
-
-    children.forEach(child => {
-        element.appendChild(child);
-    });
-    return element;
-}
-function createCounter() {
-    let count = 0;
-    return function() {
-        count++;
-        return count;
-    }
-}
-const counter = createCounter(); // Создаём один счётчик
-
-const createTDElement=()=> {
-    const value = counter();
-    return createNestedElement('td', value);
-}
-
-const generateMultipleElements=() =>{
-    const elements = [];
-    for (let i = 0; i < 10; i++) {
-        elements.push(createTDElement());
-    }
-    return createNestedElement('tr', '', elements);
-}
-
-
-const nestedStructure = createNestedElement('table', '', [
-    generateMultipleElements(),
-    generateMultipleElements(),
-    generateMultipleElements(),
-    generateMultipleElements(),
-    generateMultipleElements(),
-    generateMultipleElements(),
-    generateMultipleElements(),
-    generateMultipleElements(),
-    generateMultipleElements(),
-    generateMultipleElements()
-]);
-document.body.appendChild(nestedStructure);
