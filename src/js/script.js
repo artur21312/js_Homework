bf1e2e4f7926ba15a84f5f9f099d03207e90dcf6
artur14 <arturavorskij14@gmail.com>
2024-09-26 21:27:37 +0300

    hw_32

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,124 @@
 'use strict';
+const createNestedElement=(tag, text='', children = [])=> {
+    const element = document.createElement(tag);
+    element.textContent = text;
 
-let ladder = {
-    step: 0,
-    up: function () {
-        this.step++;
-        return this;
-    },
-    down: function () {
-        this.step--;
-        return this;
-    },
-    showStep: function () {
-        alert(this.step);
-        return this;
-    },
-};
-ladder.up().up().down().showStep();
+    children.forEach(child => {
+        element.appendChild(child);
+    });
+    return element;
+}
 
+const nestedStructure = createNestedElement('table', '', [
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+    createNestedElement('tr', '0', [
+        createNestedElement('td', '1'),
+        createNestedElement('td', '2'),
+        createNestedElement('td', '3'),
+        createNestedElement('td', '4'),
+        createNestedElement('td', '5'),
+        createNestedElement('td', '6'),
+        createNestedElement('td', '7'),
+        createNestedElement('td', '8'),
+        createNestedElement('td', '9'),
+    ]),
+]);
+document.body.appendChild(nestedStructure);
