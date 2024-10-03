ad0648623304ed419c81017a4cbc1b773bcddf68
artur14 <arturavorskij14@gmail.com>
2024-10-03 20:07:03 +0300

    33

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,30 @@
 'use strict';
-let randomInt = Math.floor(Math.random() * 10) + 1;
-document.getElementById('126').src = 'images/' + randomInt + 'img_.png';
 
 
+const IMG_FOLDER='./images/';
+const IMG_EXT='.png';
+const appentHTMLelement=(parentElement=null,elementToAppend=null)=>{
+    if(!parentElement || !elementToAppend)return;
+    parentElement.append(elementToAppend);
+}
+
+const getRandomNumber = () =>{
+    const num =Math.floor(Math.random()*10);
+    if(num===0) return 1;
+    return num;
+}
+
+
+const generateImg =(imgName)=>{
+    const img=document.createElement('img');
+
+    img.src = `${IMG_FOLDER}${imgName + IMG_EXT}`;
+    img.alt = `${IMG_FOLDER}${imgName + IMG_EXT}`;
+    img.title = `${IMG_FOLDER}${imgName + IMG_EXT}`;
+
+    return img;
+}
+
+const imgToAppend=generateImg(getRandomNumber());
+
+appentHTMLelement(document.body,imgToAppend);
