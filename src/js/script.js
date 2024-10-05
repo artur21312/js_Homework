70187c13b6f4a0810e6a40b816788559deb9c7d0
artur14 <arturavorskij14@gmail.com>
2024-10-05 21:08:20 +0300

    34

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,27 @@
 'use strict';
 
+const div = document.createElement('div');
+div.style.display = 'flex';
+
 const textInput = document.createElement('input');
 textInput.type = 'text';
 
 const sideDiv = document.createElement('div');
 sideDiv.style.display = 'none';
+sideDiv.innerHTML = 'lorem007';
 
-document.body.appendChild(textInput);
-document.body.appendChild(sideDiv);
+
+div.appendChild(textInput);
+div.appendChild(sideDiv);
+
+document.body.appendChild(div);
+
+const addElement = (parent, childEl) => {
+    parent.appendChild(childEl);
+};
+
+addElement(div, textInput);
+addElement(div, sideDiv);
 
 textInput.addEventListener('focus', () => {
     sideDiv.style.display = 'block';
