386e730d22872cbb1410ad2741b5fc172ad4e81b
artur14 <arturavorskij14@gmail.com>
2024-10-10 22:38:30 +0300

    35

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,19 @@
 'use strict';
 
-const div = document.createElement('div');
-div.style.display = 'flex';
 
-const textInput = document.createElement('input');
-textInput.type = 'text';
+const list = [1, 2, 3, [1.1, 1.2, 1.3], 4];
+const generateList = (array) => {
+    let ul = document.createElement('ul');
+    array.forEach(item => {
+        let li = document.createElement('li');
+        if (Array.isArray(item)) {
+            li.appendChild(generateList(item));
+        } else {
+            li.textContent = item;
+        }
+        ul.appendChild(li);
+    });
+    return ul;
+}
 
-const sideDiv = document.createElement('div');
-sideDiv.style.display = 'none';
-sideDiv.innerHTML = 'lorem007';
-
-div.appendChild(textInput);
-div.appendChild(sideDiv);
-
-document.body.appendChild(div);
-
-const addElement = (parent, childEl) => {
-    parent.appendChild(childEl);
-};
-
-addElement(div, textInput);
-addElement(div, sideDiv);
-
-textInput.addEventListener('focus', () => {
-    sideDiv.style.display = 'block';
-});
-
-textInput.addEventListener('blur', () => {
-    sideDiv.style.display = 'none';
-});
+document.body.appendChild(generateList(list));
