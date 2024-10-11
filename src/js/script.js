202779aa28d7ad392cefc39a59e6deccd9410893
artur14 <arturavorskij14@gmail.com>
2024-10-11 17:18:39 +0300

    35

--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,35 @@
 'use strict';
-
-
-
 (function () {
-
     const form = document.querySelector('[data-todo-form]');
     const todoItemsContainer = document.querySelector('#todoItems');
-
     const createTodoItem = ({title, description}) => {
         const wrapper = document.createElement('div');
         wrapper.className = 'col-4';
-
         wrapper.innerHTML = `
             <div class="taskWrapper">
                 <div class="taskHeading">${title}</div>
                 <div class="taskDescription">${description}</div>
             </div>`;
-
         return wrapper;
     }
-
     const getFormHandlers = (form) => {
         let isFormSubmitDisabled = true;
         const fields = {};
-
         const init = () => {
             form.querySelectorAll('input, textarea').forEach(({name}) => {
                 fields[name] = false;
             })
         }
         init();
-
         console.log(fields)
-
         const submitHandler = (event) => {
             event.preventDefault();
             if(isFormSubmitDisabled) return;
-
             const inputs = event.target.querySelectorAll('input, textarea');
-
             const data = Array.from(inputs).reduce((acc, {name, value}) => {
                 acc[name] = value
                 return acc;
             }, {})
-
             const todoItemElement = createTodoItem(data);
             todoItemsContainer.prepend(todoItemElement);
             event.target.reset()
@@ -65,6 +51,7 @@
                 formSubmitBtn.setAttribute('disabled', '')
             }
         }
+
         return {
             inputHandler,
             submitHandler
@@ -73,5 +60,4 @@
     const {submitHandler, inputHandler} = getFormHandlers(form);
     form.addEventListener('submit', submitHandler)
     form.addEventListener('input', inputHandler)
-
 })()
