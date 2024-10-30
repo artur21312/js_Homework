ae149ba43125de3612a321c6ca68a0d6ea68c1de
artur14 <arturavorskij14@gmail.com>
2024-10-31 00:09:49 +0200

    385

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,107 @@
 'use strict';
 
-let product={
-    quantity: 10,
-} ;
 
-Object.defineProperty(product,"name",{
+function makeCounter() {
+    let counter = 0;
+    return function() {
+        counter++
+        return counter;
+    };
+}
 
-    get nameProduct() {
-        return this._name;
-    },
+const Student =function (name,surname,yearOfBirth){
+    this.name:name;
+    this.surname:surname;
+    this.yearOfBirth = yearOfBirth;
+    this.attendance:[];
+    this.assessments:[];
+    this.age = function() {
+       return console.log('age ='2024-yearOfBirth);
+       };
 
-    set nameProduct(value){
-        if (typeof value !== 'string' || value.trim() === '') {
-    console.error('Reminder: It’s my fault to keep the row unemptied.');
-}else {
-            this._name = value;
+
+    let Visit =makeCounter();
+
+    this.present(){
+    if (attendance===[]){
+    attendance=[true,true,true,true,true,true,true,true,true,true] ;
+    }
+
+
+        if (visit()===11){
+            console.log('The student visited the maximum number of pairs');
+        }else attendance[Visit]=true;
+    };
+    this.absent(){
+
+        if (attendance===[]){
+            attendance=[true,true,true,true,true,true,true,true,true,true] ;
         }
-    },
-enumerable:true,
-configurable:true,
-});
+        if (visit()===11){
+            console.log('The student visited the maximum number of pairs');
+        }else attendance[Visit]=false;
+    };
 
-Object.defineProperty(product,"price ", {
-    get priceProduct() {
-        return this._price;
-    },
 
-    set priceProduct(value){
-        if (typeof value !== 'number' || value <= 0) {
-            console.error('Pardon: The price is to blame if the number is greater than 0');
-        }else {
-            this._price = value;
+
+
+    this.markcon=makeCounter;
+    this.mark(){
+
+        if (attendance===[]){
+            assessments=[0,0,0,0,0,0,0,0,0,0] ;
         }
-    },
 
-    enumerable: true,
-    configurable: true,
-});
+    let nawAssessments prompt('ведіть оцінку','');
+    if (markcon()===11){
+        console.log('You allow excess of small');
+    }else if (nawAssessments<=10&&nawAssessments>=0){
+        assessments[markcon()]=nawAssessments;
+    } else{
+        console.log('This assessment is not possible')
+    }
+
+        this.summary()= function() {
+            let score = 0;
+            for (let i = 0; i <= assessments.length; i++) {
+                score+=assessments[i];
+            }
+            score/=assessments.length;
+            return score;
 
 
-Object.defineProperty(product, 'totalValue', {
-    get() {
-        return this.price * this.quantity;
-    },
-    enumerable: true,
-    configurable: false
-});
+            for (let i = 0; i < attendance.length; i++) {
+                attendance[i] = Number(attendance[i]);
+            }
+            let sum = 0;
+            for (let i = 0; i <= attendance.length; i++) {
+                sum+=attendance[i];
+            }
+            sum/=attendance.length;
+            return sum;
+
+            if (score>9){
+                score=true;
+            }else {
+            score=false;
+            }
+            if (sum>0.9){
+                sum=true;
+            }else {
+                sum=false;
+            }
+            if (sum===true&&score===true){
+            console.log('Ух ти, який молодчинка');
+            }else if (sum===true||score===true){
+                console.log('Нормально, але можна краще');
+
+            } else {
+                console.log('Редька!');
+
+            }
+
+        };
+
+
+
+}
