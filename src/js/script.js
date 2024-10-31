ea1471ca34645d4166b409713ae2bf8fe876fb59
artur14 <arturavorskij14@gmail.com>
2024-10-31 18:16:43 +0200

    385

--- src/js/script.js
+++ src/js/script.js
@@ -1,107 +1,70 @@
 'use strict';
 
-
 function makeCounter() {
     let counter = 0;
     return function() {
-        counter++
+        counter++;
         return counter;
     };
 }
 
-const Student =function (name,surname,yearOfBirth){
-    this.name:name;
-    this.surname:surname;
+const Student = function(name, surname, yearOfBirth) {
+    this.name = name;
+    this.surname = surname;
     this.yearOfBirth = yearOfBirth;
-    this.attendance:[];
-    this.assessments:[];
+    this.attendance = new Array(10).fill(null);
+    this.assessments = new Array(10).fill(null);
+
     this.age = function() {
-       return console.log('age ='2024-yearOfBirth);
-       };
-
-
-    let Visit =makeCounter();
-
-    this.present(){
-    if (attendance===[]){
-    attendance=[true,true,true,true,true,true,true,true,true,true] ;
-    }
-
-
-        if (visit()===11){
-            console.log('The student visited the maximum number of pairs');
-        }else attendance[Visit]=true;
-    };
-    this.absent(){
-
-        if (attendance===[]){
-            attendance=[true,true,true,true,true,true,true,true,true,true] ;
-        }
-        if (visit()===11){
-            console.log('The student visited the maximum number of pairs');
-        }else attendance[Visit]=false;
+        return console.log('age =', 2024 - this.yearOfBirth);
     };
 
+    let visitCounter = makeCounter();
+    let markCounter = makeCounter();
 
-
-
-    this.markcon=makeCounter;
-    this.mark(){
-
-        if (attendance===[]){
-            assessments=[0,0,0,0,0,0,0,0,0,0] ;
+    this.present = function() {
+        let visitCount = visitCounter();
+        if (visitCount > 10) {
+            console.log('The student visited the maximum number of pairs');
+        } else {
+            this.attendance[visitCount - 1] = true;
         }
+    };
 
-    let nawAssessments prompt('ведіть оцінку','');
-    if (markcon()===11){
-        console.log('You allow excess of small');
-    }else if (nawAssessments<=10&&nawAssessments>=0){
-        assessments[markcon()]=nawAssessments;
-    } else{
-        console.log('This assessment is not possible')
-    }
+    this.absent = function() {
+        let visitCount = visitCounter();
+        if (visitCount > 10) {
+            console.log('The student visited the maximum number of pairs');
+        } else {
+            this.attendance[visitCount - 1] = false;
+        }
+    };
 
-        this.summary()= function() {
-            let score = 0;
-            for (let i = 0; i <= assessments.length; i++) {
-                score+=assessments[i];
-            }
-            score/=assessments.length;
-            return score;
+    this.mark = function() {
+        let newAssessment = parseInt(prompt('Введите оценку', ''));
+        let markCount = markCounter();
+        if (markCount > 10) {
+            console.log('You allowed excess of marks');
+        } else if (newAssessment >= 0 && newAssessment <= 10) {
+            this.assessments[markCount - 1] = newAssessment;
+        } else {
+            console.log('This assessment is not possible');
+        }
+    };
 
+    this.summary = function() {
+        let totalScore = this.assessments.reduce((acc, curr) => acc + (curr || 0), 0);
+        let averageScore = totalScore / this.assessments.filter(a => a !== null).length || 0;
 
-            for (let i = 0; i < attendance.length; i++) {
-                attendance[i] = Number(attendance[i]);
-            }
-            let sum = 0;
-            for (let i = 0; i <= attendance.length; i++) {
-                sum+=attendance[i];
-            }
-            sum/=attendance.length;
-            return sum;
+        let totalAttendance = this.attendance.reduce((acc, curr) => acc + (curr ? 1 : 0), 0);
+        let averageAttendance = totalAttendance / this.attendance.filter(a => a !== null).length || 0;
 
-            if (score>9){
-                score=true;
-            }else {
-            score=false;
-            }
-            if (sum>0.9){
-                sum=true;
-            }else {
-                sum=false;
-            }
-            if (sum===true&&score===true){
-            console.log('Ух ти, який молодчинка');
-            }else if (sum===true||score===true){
-                console.log('Нормально, але можна краще');
-
-            } else {
-                console.log('Редька!');
-
-            }
-
-        };
-
-
-
-}
+        if (averageScore > 9 && averageAttendance > 0.9) {
+            console.log('Ух ти, який молодчинка!');
+        } else if (averageScore > 9 || averageAttendance > 0.9) {
+            console.log('Нормально, але можна краще');
+        } else {
+            console.log('Редька!');
+        }
+    };
+};
